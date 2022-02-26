048434d5f4df71a18c3fc58d65c3f17222e6ebf1
Cho SangHyun <98shcho@naver.com>
2022-02-26 12:25:09 +0900

    improve: Change the mock-data to JSX elements and mix them
    So I tried to make a method that changes the state value(selectedIndex) as an onClick property
    when creating the DessertItem component through mock-data. For this, I used "useEffect" but i
    meet one problem that called "missing dependency". In "useEffect", the method that change the state
    value is given as the prop of DessertItem, I think it seems not be the way. So now I`m thinking about how to solve this problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,51 @@
+import { useEffect, useState } from "react";
 import mock from "../mock.json";
 import DessertItem from "./DessertItem";
+import './App.css';
+
+// 피셔 - 예이츠 셔플(배열 섞는 알고리즘)
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        // destructuring 문법
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 function App(){
-    console.log(mock);
+    // 디저트들이 JSX형태로 담긴 배열
+    const [dessertList, setDessertList] = useState([])
+    // dessertList를 순회하는 인덱스 역할
+    const [selectIndex, setSelectIndex] = useState(0);
+    // 사용자로부터 선택된 디저트들이 담기는 배열
+    // const [selectedList, setSelectedList] = useState([]);
+
+    const selectNextDessert = () => {
+        let nextSelectIndex = selectIndex + 2;
+        setSelectIndex(nextSelectIndex);
+        console.log(`index : ${selectIndex}`);
+    };
+
+    useEffect(() => {
+        shuffle(mock);
+
+        const dessert_list = mock.map((dessert) => {
+            return(
+                <li key={dessert.id}>
+                    <DessertItem onSelect={selectNextDessert} item={dessert}/>
+                </li>
+            )
+        })
+
+        console.log(dessert_list);
+        setDessertList(dessert_list);
+    }, []);
+
     return(
         <div>
-            {mock.map((dessert) => {
-                return(
-                    <li key={dessert.id}>
-                        <DessertItem item={dessert}/>
-                    </li>
-                )
-            })}
+            <div className="select-zone">
+                {dessertList}
+            </div>
         </div>
     )
 }
